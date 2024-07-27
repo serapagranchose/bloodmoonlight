ffc959f893b8a904c5374213a619909991d797ee
Charles Dubret <charles.dubret@epitech.eu>
2024-07-27 15:41:59 +0200

    Player movement

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -1,27 +1,29 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 120.0
 const JUMP_VELOCITY = -400.0
 
-class PlayerDirection:
-	const down  = [0, -1]
-	const left = [-1, 0]
-	const right = [1, 0]
-	const up = [0, 1]
-
+enum PlayerDirection {
+	down,
+	left,
+	right,
+	up
+}
 
 var leftPressed = false
 
-var _direction = PlayerDirection.down
+var _direction: PlayerDirection = PlayerDirection.down
 var _directions: Array = []
 @onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	
+
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	print(input_direction)
+	velocity = input_direction * SPEED
 	set_player_animation()
-	velocity.x = _direction[0] * SPEED
-	velocity.y = _direction[1] * SPEED
+	move_and_slide()
 	
 func set_player_animation():
 	if (Input.is_action_pressed("ui_left") && _direction != PlayerDirection.left):
@@ -29,17 +31,17 @@ func set_player_animation():
 		_directions.push_front(PlayerDirection.left)
 		_direction = PlayerDirection.left
 		_animated_sprite.play("run_left")
-	elif (Input.is_key_pressed(KEY_D) && _direction != PlayerDirection.right):
+	elif (Input.is_action_pressed("ui_right") && _direction != PlayerDirection.right):
 		print("Input right")
 		_directions.push_front(PlayerDirection.right)
 		_direction = PlayerDirection.right
 		_animated_sprite.play("run_right")
-	elif (Input.is_key_pressed(KEY_Z) && _direction != PlayerDirection.up):
+	elif (Input.is_action_pressed("ui_up") && _direction != PlayerDirection.up):
 		print("Input up")
 		_directions.push_front(PlayerDirection.up)
 		_direction = PlayerDirection.up
 		_animated_sprite.play("run_up")
-	elif (Input.is_key_pressed(KEY_S) && _direction != PlayerDirection.down):
+	elif (Input.is_action_pressed("ui_down") && _direction != PlayerDirection.down):
 		print("Input down")
 		_directions.push_front(PlayerDirection.down)
 		_direction = PlayerDirection.down
@@ -50,7 +52,7 @@ func set_player_animation():
 	if (_directions.size() > 5):
 		_directions.pop_back()
 
-func get_idle_animation(direction) -> String:
+func get_idle_animation(direction: PlayerDirection) -> String:
 	var idle_animation = ""
 	match direction:
 		PlayerDirection.left: idle_animation = "idle_left"
@@ -74,4 +76,4 @@ func get_animation_priority() -> String:
 		highest_direction = direction_count.keys()[0]
 	else:
 		highest_direction = "None"
-	return get_idle_animation(highest_direction)
+	return get_idle_animation(highest_direction as PlayerDirection)
