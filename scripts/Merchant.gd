adbe403fd165e8a635e36d85b047f76f60801d33
serapagranchose <seraphin.perrot@gmail.com>
2024-07-28 21:42:22 +0200

    Adding Dialog for the merchant

--- scripts/Merchant.gd
+++ scripts/Merchant.gd
@@ -2,8 +2,14 @@ extends CharacterBody2D
 
 @onready var interaction_area = $InteractionArea
 
+const lines = [
+	"HELLOOO!",
+	"HOW YOU DOING?"
+]
+
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 
 func _on_interact():
-	print("helloooo")
+	DialogManager.start_dialog(global_position, lines)
+	await DialogManager.dialog_finished
